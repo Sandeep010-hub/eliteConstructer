4abc5a959f89c55e1f7477e1850583a84b0b7048
Yaswanth Sandeep Sai Kalagatla <135518346+Sandeep010-hub@users.noreply.github.com>
2025-07-20 10:28:42 +0530

    Updated Blog.tsx

--- src/components/sections/Blog.tsx
+++ src/components/sections/Blog.tsx
@@ -4,7 +4,7 @@ import { CalendarIcon, ClockIcon, UserIcon } from '@heroicons/react/24/outline';
 import { Section } from '../ui/Section';
 import { Card } from '../ui/Card';
 import { Button } from '../ui/Button';
-import { blogPosts } from '../../data/blog';
+import { blogPosts } from '../data/blog';
 
 export const Blog: React.FC = () => {
   return (
